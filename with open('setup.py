69c6a5c9ad0a113333d81fcbacd75bb84dbeb262
kho-ren <57721262+kho-ren@users.noreply.github.com>
2024-04-08 11:38:30 +0400

    Unnecessary File

--- with open('setup.py
+++
@@ -1,7 +0,0 @@
-with open('setup.py', 'r', encoding='utf-8') as file:
-    content = file.read()
-
-clean_content = content.encode('utf-8', 'ignore').decode('utf-8')
-
-with open('setup.py', 'w', encoding='utf-8') as file:
-    file.write(clean_content)
