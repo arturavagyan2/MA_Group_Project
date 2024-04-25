6f1903cca72628acce8dfec25c5d398e0eda329d
Ina Karapetyan <ina_karapetyan@bk.ru>
2024-04-25 14:51:31 +0400

    New method and function descriptions added

--- SuRFM/db/sql_handler.py
+++ SuRFM/db/sql_handler.py
@@ -13,33 +13,69 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(CustomFormatter())
 logger.addHandler(ch)
 
+
 class SqlHandler:
+
     def __init__(self, dbname: str, table_name: str) -> None:
+        """
+        Initializes a new instance of the SqlHandler class.
+
+        Parameters:
+            dbname (str): The name of the SQLite database file.
+            table_name (str): The name of the table in the database to interact with.
+        """
+
         self.cnxn = sqlite3.connect(f'{dbname}.db')
         self.cursor = self.cnxn.cursor()
         self.dbname = dbname
         self.table_name = table_name
-
+#______________________________________________________________________________________________
+        
     def close_cnxn(self) -> None:
+        """
+        Closes the database connection.
+
+        This method should be called when finished with database operations.
+        """
         logger.info('Committing the changes')
         self.cursor.close()
         self.cnxn.close()
         logger.info('The connection has been closed')
-
+#______________________________________________________________________________________________
+    
     def get_table_columns(self) -> list:
+        """
+        Retrieves the column names of the specified table.
+
+        Returns:
+            list: A list of column names.
+        """
         self.cursor.execute(f"PRAGMA table_info({self.table_name});")
         columns = self.cursor.fetchall()
         column_names = [col[1] for col in columns]
         logger.info(f'The list of columns: {column_names}')
         return column_names
+#______________________________________________________________________________________________
     
     def truncate_table(self) -> None:
+        """
+        Drops the table if it exists.
+
+        This method deletes all data and structure from the table.
+        """
         query = f"DROP TABLE IF EXISTS {self.table_name};"
         self.cursor.execute(query)
         logger.info(f'The {self.table_name} table is truncated')
         self.cnxn.commit()
-
+#______________________________________________________________________________________________
+    
     def insert_many(self, df: pd.DataFrame) -> None:
+        """
+        Inserts data from a DataFrame into the database table.
+
+        Parameters:
+            df (pd.DataFrame): The DataFrame containing the data to insert.
+        """
         df = df.replace(np.nan, None)  
         df.rename(columns=lambda x: x.lower(), inplace=True)
         columns = list(df.columns)
@@ -66,7 +102,8 @@ class SqlHandler:
         self.cursor.executemany(query, values)
         self.cnxn.commit()
         logger.warning('The data is loaded')
-
+#______________________________________________________________________________________________
+    
     def update_table(self, set_clause: str, condition: str) -> None:
         """
         Update rows in the table based on a condition.
@@ -79,3 +116,31 @@ class SqlHandler:
         self.cursor.execute(query)
         self.cnxn.commit()
         logger.info(f"Updated rows in '{self.table_name}' where {condition}.")
+#______________________________________________________________________________________________
+      
+    def get_table_data(self, columns: list = None, condition: str = None) -> pd.DataFrame:
+        """
+        Retrieve data from the table based on the specified columns and optional condition.
+
+        Parameters:
+            columns (list): A list of column names to retrieve. If not specified, all columns will be retrieved.
+            condition (str): An optional SQL condition to filter the data (e.g., "column_name = 'value'"). If not specified, all data will be retrieved.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the retrieved data.
+        """
+        if columns is None:
+            columns = self.get_table_columns()
+
+        if not columns:
+            return pd.DataFrame()
+
+        column_names = ', '.join(columns)
+
+        query = f"SELECT {column_names} FROM {self.table_name}"
+        if condition:
+            query += f" WHERE {condition}"
+
+        data = pd.read_sql_query(query, self.cnxn)
+        return data
+   
