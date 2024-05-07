c5fe73f10de532a0ab0c4bb327d5bef1fe86fdd6
Ina Karapetyan <ina_karapetyan@bk.ru>
2024-05-07 22:15:02 +0400

    Too long lines so #noqa-s added

--- calculate_clv.py
+++ calculate_clv.py
@@ -9,17 +9,17 @@ if __name__ == "__main__":
     subscribers = connect_to_subscribers.get_table_data()
     connect_to_subscribers.close_cnxn()
 
-    subscribers['clv_value'] = [CLV(t=5, customer_id=id).calculate_clv() for id in subscribers['subscriber_id']]
-    subscribers['clv_date'] = pd.Timestamp.now()  
+    subscribers['clv_value'] = [CLV(t=5, customer_id=id).calculate_clv() for id in subscribers['subscriber_id']]  # noqa: E501
+    subscribers['clv_date'] = pd.Timestamp.now()
 
-    prediction = subscribers[['subscriber_id', 'clv_value', 'clv_date']].reset_index()
-    prediction.rename(columns={'index': 'clv_id', 'subscriber_id': 'subscriber_id', 'clv_value': 'clv_value'}, inplace=True)
+    prediction = subscribers[['subscriber_id', 'clv_value', 'clv_date']].reset_index()  # noqa: E501
+    prediction.rename(columns={'index': 'clv_id', 'subscriber_id': 'subscriber_id', 'clv_value': 'clv_value'}, inplace=True)  # noqa: E501
 
     labels = ["New Potential", "Emerging", "Established", "Champion"]
-    prediction['predicted_type'] = pd.qcut(prediction['clv_value'], 4, labels=labels)
+    prediction['predicted_type'] = pd.qcut(prediction['clv_value'], 4, labels=labels)  # noqa: E501
 
-    prediction['is_success'] = prediction['predicted_type'].apply(lambda x: True if x == "Star" else False)
+    prediction['is_success'] = prediction['predicted_type'].apply(lambda x: True if x == "Star" else False)  # noqa: E501
 
     connect_to_prediction = SqlHandler('subscription_database', 'clv')
-    connect_to_prediction.insert_many(prediction.to_dict('records'))  
+    connect_to_prediction.insert_many(prediction.to_dict('records'))
     connect_to_prediction.close_cnxn()
