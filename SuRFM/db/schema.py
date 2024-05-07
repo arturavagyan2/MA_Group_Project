026ae2a56fc12eb5906e02629944360340aeaa4e
Ina Karapetyan <ina_karapetyan@bk.ru>
2024-05-07 22:04:06 +0400

    flake8 conflicts solved

--- SuRFM/db/schema.py
+++ SuRFM/db/schema.py
@@ -1,8 +1,11 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, DateTime, ForeignKey
+from sqlalchemy import create_engine, \
+    Column, Integer, String, Float, Boolean, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+
 Base = declarative_base()
+
 
 class Subscriber(Base):
     __tablename__ = 'subscriber'
@@ -19,8 +22,9 @@ class Subscriber(Base):
     event_observed = Column(Boolean)
     activities = relationship('Activity', back_populates='subscriber')
     transactions = relationship('Transaction', back_populates='subscriber')
-    rfm_segmentation = relationship('RFMSegmentation', back_populates='subscriber', uselist=False)
+    rfm_segmentation = relationship('RFMSegmentation', back_populates='subscriber', uselist=False)  # noqa: E501
     clv = relationship('CLV', back_populates='subscriber', uselist=False)
+
 
 class Activity(Base):
     __tablename__ = 'activity'
@@ -31,6 +35,7 @@ class Activity(Base):
     activity_date_time = Column(DateTime)
     subscriber = relationship('Subscriber', back_populates='activities')
 
+
 class Transaction(Base):
     __tablename__ = 'transactions'
 
@@ -38,9 +43,10 @@ class Transaction(Base):
     subscriber_id = Column(Integer, ForeignKey('subscriber.subscriber_id'))
     transaction_date = Column(DateTime)
     transaction_amount = Column(Float)
-    payment_method_id = Column(Integer, ForeignKey('payment_method.payment_method_id'))
+    payment_method_id = Column(Integer, ForeignKey('payment_method.payment_method_id'))  # noqa: E501
     subscriber = relationship('Subscriber', back_populates='transactions')
     payment_method = relationship('PaymentMethod')
+
 
 class PaymentMethod(Base):
     __tablename__ = 'payment_method'
@@ -48,6 +54,7 @@ class PaymentMethod(Base):
     payment_method_id = Column(Integer, primary_key=True)
     payment_name = Column(String)
     transactions = relationship('Transaction', back_populates='payment_method')
+
 
 class RFMSegmentation(Base):
     __tablename__ = 'rfm_segmentation'
@@ -57,9 +64,9 @@ class RFMSegmentation(Base):
     recency_score = Column(Float)
     frequency_score = Column(Float)
     monetary_score = Column(Float)
-    segment_label = Column(String)
     subscriber = relationship('Subscriber', back_populates='rfm_segmentation')
-    retention_strategy = relationship('RetentionStrategy', back_populates='rfm_segmentation', uselist=False)
+    retention_strategy = relationship('RetentionStrategy', back_populates='rfm_segmentation', uselist=False)  # noqa: E501
+
 
 class RetentionStrategy(Base):
     __tablename__ = 'retention_strategy'
@@ -70,7 +77,8 @@ class RetentionStrategy(Base):
     description = Column(String)
     performance_metrics = Column(String)
     status = Column(String)
-    rfm_segmentation = relationship('RFMSegmentation', back_populates='retention_strategy')
+    rfm_segmentation = relationship('RFMSegmentation', back_populates='retention_strategy')  # noqa: E501
+
 
 class CLV(Base):
     __tablename__ = 'clv'
@@ -83,6 +91,7 @@ class CLV(Base):
     subscriber_id = Column(Integer, ForeignKey('subscriber.subscriber_id'))
     subscriber = relationship('Subscriber', back_populates='clv')
 
+
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///subscription_database.db')  
+    engine = create_engine('sqlite:///subscription_database.db')
     Base.metadata.create_all(engine)
