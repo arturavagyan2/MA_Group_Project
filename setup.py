bb3663bccbebbdf394dd7e65e4e95749f8b4845b
Ina Karapetyan <ina_karapetyan@bk.ru>
2024-05-07 22:30:29 +0400

    Flake8 solved

--- setup.py
+++ setup.py
@@ -8,20 +8,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 # List of required packages
 required_packages = [
-    'annotated-types', 'anyio', 'exceptiongroup', 'Faker', 'fastapi', 'greenlet', 'idna', 'numpy', 'pandas',
-    'pydantic', 'pydantic_core', 'python-dateutil', 'pytz', 'six', 'sniffio', 'SQLAlchemy', 'starlette',
-    'typing_extensions', 'tzdata', 'appnope', 'asttokens', 'certifi', 'charset-normalizer', 'click', 'comm',
-    'debugpy', 'decorator', 'executing', 'h11', 'httptools', 'importlib_metadata', 'ipykernel', 'ipython', 'jedi',
-    'jupyter_client', 'jupyter_core', 'matplotlib-inline', 'nest-asyncio', 'packaging', 'parso', 'pexpect',
-    'platformdirs', 'prompt-toolkit', 'psutil', 'ptyprocess', 'pure-eval', 'Pygments', 'python-dotenv', 'PyYAML',
-    'pyzmq', 'requests', 'stack-data', 'tornado', 'traitlets', 'urllib3', 'uvicorn', 'uvloop', 'watchfiles',
-    'wcwidth', 'websockets', 'zipp'
+    'annotated-types', 'anyio', 'exceptiongroup', 'Faker', 'fastapi',
+    'greenlet', 'idna', 'numpy', 'pandas', 'pydantic', 'pydantic_core',
+    'python-dateutil', 'pytz', 'six', 'sniffio', 'SQLAlchemy', 'starlette',
+    'typing_extensions', 'tzdata', 'appnope', 'asttokens', 'certifi',
+    'charset-normalizer', 'click', 'comm', 'debugpy', 'decorator', 'executing',
+    'h11', 'httptools', 'importlib_metadata', 'ipykernel', 'ipython',
+    'jedi', 'jupyter_client', 'jupyter_core', 'matplotlib-inline',
+    'nest-asyncio', 'packaging', 'parso', 'pexpect', 'platformdirs',
+    'prompt-toolkit', 'psutil', 'ptyprocess', 'pure-eval', 'Pygments',
+    'python-dotenv', 'PyYAML', 'pyzmq', 'requests', 'stack-data', 'tornado',
+    'traitlets', 'urllib3', 'uvicorn', 'uvloop', 'watchfiles', 'wcwidth',
+    'websockets', 'zipp'
 ]
 
 # Call setup function
 setup(
-    author="Artur Avagyan, Elen Galoyan, Hrag Sousani, Ina Karapetyan, Khoren Movsisyan",
-    description="SuRFM is a Python package for conducting RFM (Recency, Frequency, Monetary) analysis of customers. It provides tools for segmenting customers based on their transaction behavior and identifying high-value segments.",
+    author="Artur Avagyan, Elen Galoyan, Hrag Sousani, Ina Karapetyan, Khoren Movsisyan",  # noqa: E501
+    description="SuRFM is a Python package for conducting RFM (Recency, Frequency, Monetary) analysis of customers. It provides tools for segmenting customers based on their transaction behavior and identifying high-value segments.",  # noqa: E501
     long_description=long_description,
     long_description_content_type="text/markdown",
     name="SuRFM",
